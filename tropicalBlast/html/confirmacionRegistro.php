304b35739fc35b2cc8fd91a816494ca699b1977c
MaykelRobles <82823403+MaykelRobles@users.noreply.github.com>
2021-05-29 16:35:46 -0600

    php
    update

--- tropicalBlast/html/confirmacionRegistro.php
+++ tropicalBlast/html/confirmacionRegistro.php
@@ -88,9 +88,8 @@
                     <div class="row">
                         
                     <div class="col">
-                    <label type="submit" class="textRegistro">
                                 <input class="obtenerInfo text" type="number" name="tarjeta" id="tarjeta" placeholder="Tarjeta" aria-label="Tarjeta"></input>
-                            </label>
+                
                         </div>
 
                         <div class="col-3">
